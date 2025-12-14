0e058ef64addcc9a53a752e80a255e10fdccce20
Kai Siang Kao <work@ks-kao.com>
2025-12-14 22:14:46 +0800

    Add tanstack query

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,26 @@
 <script lang="ts">
 	import './layout.css';
+	import { QueryClient, QueryClientProvider } from '@tanstack/svelte-query';
 	import favicon from '$lib/assets/favicon.svg';
+	import { Toaster } from '$lib/components/ui/sonner';
+	import { toast } from 'svelte-sonner';
 
 	let { children } = $props();
+	const queryClient = new QueryClient({
+		defaultOptions: {
+			mutations: {
+				onError: (e) => {
+					toast.error(e.message);
+				}
+			}
+		}
+	});
 </script>
 
-<svelte:head><link rel="icon" href={favicon} /></svelte:head>
-{@render children()}
+<svelte:head>
+	<link rel="icon" href={favicon} />
+</svelte:head>
+<QueryClientProvider client={queryClient}>
+	<Toaster richColors position="top-center" />
+	{@render children()}
+</QueryClientProvider>
